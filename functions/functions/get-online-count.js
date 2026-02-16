13f5f266968b32daa15b720918e30ee74e456854
Fils25git <fleduportal25@gmail.com>
2026-02-16 05:35:19 +0200

    Create get-online-count.js

---
+++ functions/functions/get-online-count.js
@@ -0,0 +1,36 @@
+const { Client } = require("pg");
+
+exports.handler = async () => {
+
+  const client = new Client({
+    connectionString: process.env.NEON_DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  try {
+    await client.connect();
+
+    const result = await client.query(
+      `
+      SELECT COUNT(*) 
+      FROM active_users
+      WHERE last_active > NOW() - INTERVAL '2 minutes';
+      `
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        onlineUsers: result.rows[0].count
+      }),
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  } finally {
+    await client.end();
+  }
+};
