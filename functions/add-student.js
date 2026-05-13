1701adf45717fbc1249cac6c8275a71466761a1e
Fils25git <fleduportal25@gmail.com>
2026-05-13 19:14:01 +0200

    Add function to insert a new student into the database
    This function handles the addition of a new student to the database by inserting their details and returning the added student information.

---
+++ functions/add-student.js
@@ -0,0 +1,33 @@
+const db = require("./db");
+
+exports.handler = async (event) => {
+  try {
+    const {
+      full_name,
+      gender,
+      reg_number,
+      class_id
+    } = JSON.parse(event.body);
+
+    const result = await db.query(
+      `INSERT INTO students (full_name, gender, reg_number, class_id)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [full_name, gender, reg_number, class_id]
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Student added",
+        student: result.rows[0]
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
