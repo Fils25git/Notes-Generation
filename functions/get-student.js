478b236040a0afec92ae37442922a88308909e2f
Fils25git <fleduportal25@gmail.com>
2026-05-13 19:14:59 +0200

    Implement get-student function to query students

---
+++ functions/get-student.js
@@ -0,0 +1,31 @@
+const db = require("./db");
+
+exports.handler = async (event) => {
+  try {
+    const class_id = event.queryStringParameters?.class_id;
+
+    let result;
+
+    if (class_id) {
+      result = await db.query(
+        "SELECT * FROM students WHERE class_id = $1 ORDER BY id ASC",
+        [class_id]
+      );
+    } else {
+      result = await db.query(
+        "SELECT * FROM students ORDER BY id ASC"
+      );
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.rows)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
