47269e2fdecf350f11f9df642e4ddfa42a101b47
Fils25git <fleduportal25@gmail.com>
2026-05-13 21:16:50 +0200

    Update get-DashboardStats.js

--- functions/get-DashboardStats.js
+++ functions/get-DashboardStats.js
@@ -3,18 +3,18 @@ const db = require("./db");
 exports.handler = async () => {
   try {
 
-    const students = await db.query("SELECT COUNT(*) FROM students");
+    const students = await db.query("SELECT COUNT(*) FROM learners");
     const classes = await db.query("SELECT COUNT(*) FROM classes");
     const subjects = await db.query("SELECT COUNT(*) FROM subjects");
-    const tests = await db.query("SELECT COUNT(*) FROM tests");
+    const tests = await db.query("SELECT COUNT(*) FROM my_tests");
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        students: students.rows[0].count,
+        learners: students.rows[0].count,
         classes: classes.rows[0].count,
         subjects: subjects.rows[0].count,
-        tests: tests.rows[0].count
+        my_tests: tests.rows[0].count
       })
     };
 
