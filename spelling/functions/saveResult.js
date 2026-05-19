4a6862a9a5f57b5362f733eb27636c3d8c4c2303
Fils25git <fleduportal25@gmail.com>
2026-05-19 22:03:29 +0200

    Add saveResult function to handle word attempts

---
+++ spelling/functions/saveResult.js
@@ -0,0 +1,71 @@
+const sql=require("./db");
+
+exports.handler=async(event)=>{
+
+try{
+
+const data=
+JSON.parse(event.body);
+
+const result=
+await sql`
+
+INSERT INTO word_attempts(
+
+student_id,
+competition_id,
+round_number,
+word,
+learner_answer,
+score,
+time_allowed,
+time_used,
+status
+
+)
+
+VALUES(
+
+${data.studentId},
+${data.competitionId},
+${data.round},
+${data.word},
+${data.answer},
+${data.score},
+${data.timeAllowed},
+${data.timeUsed},
+${data.status}
+
+)
+
+RETURNING *
+`;
+
+return{
+
+statusCode:200,
+
+body:JSON.stringify({
+success:true,
+result
+})
+
+};
+
+}
+
+catch(error){
+
+return{
+
+statusCode:500,
+
+body:JSON.stringify({
+error:error.message
+})
+
+};
+
+}
+
+};
