cae225ca25d3245f8bb11db63301b57af5d190bf
Fils25git <fleduportal25@gmail.com>
2026-05-19 22:06:32 +0200

    Add leaderboard function to fetch student scores

---
+++ spelling/functions/leaderboard.js
@@ -0,0 +1,65 @@
+const sql=require("./db");
+
+exports.handler=async()=>{
+
+try{
+
+const leaderboard=
+await sql`
+
+SELECT
+
+s.full_name,
+
+SUM(w.score)
+AS total_score
+
+FROM students s
+
+LEFT JOIN word_attempts w
+
+ON s.id=
+w.student_id
+
+GROUP BY
+s.id,
+s.full_name
+
+ORDER BY
+total_score DESC
+
+`;
+
+return{
+
+statusCode:200,
+
+body:JSON.stringify({
+
+success:true,
+leaderboard
+
+})
+
+};
+
+}
+
+catch(error){
+
+return{
+
+statusCode:500,
+
+body:JSON.stringify({
+
+success:false,
+error:error.message
+
+})
+
+};
+
+}
+
+};
