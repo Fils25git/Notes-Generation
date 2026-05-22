3f06766a9708c6a51c9e5d8eb79aa5e9d0b601a7
Fils25git <fleduportal25@gmail.com>
2026-05-22 11:00:44 +0200

    Update leaderboard.js

--- spelling/js/leaderboard.js
+++ spelling/js/leaderboard.js
@@ -8,32 +8,112 @@ await fetch(
 const data=
 await res.json();
 
+let round=
+parseInt(
+
+localStorage.getItem(
+"currentRound"
+) || 1
+
+);
+
+
+if(round<=3){
+
+document.getElementById(
+"title"
+).innerText=
+
+"YOUNG SPELLERS Contest Ranking after Round "
++ round;
+
+}
+else{
+
+document.getElementById(
+"title"
+).innerText=
+
+"YOUNG SPELLERS Contest Ranking";
+
+document.getElementById(
+"nextRoundBtn"
+).style.display=
+"none";
+
+}
+
+
 let html="";
 
 data.leaderboard.forEach((s,i)=>{
 
 let cls="";
+let rank=i+1;
 
-if(i===0) cls="top1";
-if(i===1) cls="top2";
-if(i===2) cls="top3";
+
+if(i===0){
+
+cls="top1";
+rank="🥇";
+
+}
+
+if(i===1){
+
+cls="top2";
+rank="🥈";
+
+}
+
+if(i===2){
+
+cls="top3";
+rank="🥉";
+
+}
+
 
 html+=`
+
 <tr class="${cls}">
 
-<td>${i+1}</td>
-<td>${s.full_name}</td>
-<td>${s.class_name}</td>
-<td>${s.total_score}</td>
+<td class="rank">
+
+${rank}
+
+</td>
+
+<td>
+
+${s.full_name}
+
+</td>
+
+<td>
+
+${s.class_name}
+
+</td>
+
+<td>
+
+${s.total_score}
+
+</td>
 
 </tr>
+
 `;
 
 });
 
-document.getElementById("list")
-.innerHTML=html;
+
+document.getElementById(
+"list"
+).innerHTML=html;
 
 }
+
 
 load();
