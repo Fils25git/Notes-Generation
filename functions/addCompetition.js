6521d9c9d8243166f2bb86c141a139124379ac15
Fils25git <fleduportal25@gmail.com>
2026-05-23 08:55:10 +0200

    Create addCompetition.js

---
+++ functions/addCompetition.js
@@ -0,0 +1,69 @@
+const pool=require("./spellingDb");
+
+exports.handler=async(event)=>{
+
+try{
+
+const body=
+JSON.parse(event.body);
+
+const competition_name=
+body.competition_name;
+
+if(!competition_name){
+
+return{
+statusCode:400,
+body:JSON.stringify({
+message:"Competition name required"
+})
+};
+
+}
+
+await pool.query(
+
+`
+INSERT INTO competitions
+(competition_name,status)
+
+VALUES($1,$2)
+`,
+
+[
+competition_name,
+"inactive"
+]
+
+);
+
+return{
+
+statusCode:200,
+
+body:JSON.stringify({
+
+message:"Competition created"
+
+})
+
+};
+
+}
+catch(error){
+
+return{
+
+statusCode:500,
+
+body:JSON.stringify({
+
+message:error.message
+
+})
+
+};
+
+}
+
+};
