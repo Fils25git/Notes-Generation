fa61e5fe9a4b4d0d58f64ed0a672d31b57436199
Fils25git <fleduportal25@gmail.com>
2026-05-23 10:56:13 +0200

    Update getCompetitionState.js

--- functions/getCompetitionState.js
+++ functions/getCompetitionState.js
@@ -1,48 +1,36 @@
-const sql=
-require("./spellingDb");
+const sql=require("./spellingDb");
 
-exports.handler=
-async()=>{
+exports.handler=async(event)=>{
 
 try{
 
-const state=
-await sql`
+const competition_id=
+event.queryStringParameters.competition_id;
 
+const result=
+await sql(
+`
 SELECT *
-
 FROM competition_state
-
+WHERE competition_id=$1
 LIMIT 1
-
-`;
-
+`,
+[competition_id]
+);
 
 return{
-
 statusCode:200,
-
 body:JSON.stringify({
-
-state:state[0]
-
+state:result[0] || null
 })
-
 };
 
 }
 catch(error){
 
 return{
-
 statusCode:500,
-
-body:JSON.stringify({
-
-error:error.message
-
-})
-
+body:JSON.stringify({message:error.message})
 };
 
 }
