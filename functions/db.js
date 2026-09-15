075198265b4196a2c4e02f8f120f58840a798528
Fils25git <fleduportal25@gmail.com>
2026-09-15 09:29:47 +0200

    Refactor database connection to use named import

--- functions/db.js
+++ functions/db.js
@@ -1,12 +1,10 @@
-import pg from "pg";
+import { Pool } from "pg";
 
-const { Pool } = pg;
-
-const pool = new Pool({
-  connectionString: process.env.NEON_DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const db = new Pool({
+    connectionString: process.env.NEON_DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-export default pool;
+export default db;
