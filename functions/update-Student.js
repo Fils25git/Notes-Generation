62edd1ded2425843be91b715666e3bf0f271fd9f
Fils25git <fleduportal25@gmail.com>
2026-09-15 09:01:20 +0200

    Refactor update-Student.js to use ES modules

--- functions/update-Student.js
+++ functions/update-Student.js
@@ -1,90 +1,46 @@
-const { Pool }=
-require(
-"pg"
-);
+import db from "./db.js";
 
-const db=
-new Pool({
+export const handler = async (event) => {
+    try {
+        const {
+            id,
+            full_name,
+            gender,
+            class_id
+        } = JSON.parse(event.body || "{}");
 
-connectionString:
-process.env.DATABASE_URL,
+        await db.query(
+            `
+            UPDATE learners
+            SET
+                full_name = $1,
+                gender = $2,
+                class_id = $3
+            WHERE id = $4
+            `,
+            [
+                full_name,
+                gender,
+                class_id,
+                id
+            ]
+        );
 
-ssl:{
-rejectUnauthorized:false
-}
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Student updated"
+            })
+        };
 
-});
+    } catch (error) {
+        console.error("Update learner error:", error);
 
-
-exports.handler=
-async(event)=>{
-
-try{
-
-const{
-
-id,
-full_name,
-gender,
-class_id
-
-}=
-JSON.parse(
-event.body
-);
-
-
-await db.query(
-
-`
-UPDATE learners
-SET
-
-full_name=$1,
-gender=$2,
-class_id=$3
-
-WHERE id=$4
-`,
-
-[
-full_name,
-gender,
-class_id,
-id
-]
-
-);
-
-return{
-
-statusCode:200,
-
-body:JSON.stringify({
-
-message:
-"Student updated"
-
-})
-
-};
-
-}
-catch(error){
-
-return{
-
-statusCode:500,
-
-body:JSON.stringify({
-
-error:
-error.message
-
-})
-
-};
-
-}
-
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        };
+    }
 };
