89cd40141ac019df3f74c00c78d534af3bed1575
Fils25git <fleduportal25@gmail.com>
2026-09-15 06:53:54 +0200

    Refactor get-Teachers.js to use ES6 imports

--- functions/get-Teachers.js
+++ functions/get-Teachers.js
@@ -1,6 +1,6 @@
-const db = require("./db");
+import db from "./db.js";
 
-exports.handler = async () => {
+export const handler = async (event) => {
   try {
     const result = await db.query(
       "SELECT id, full_name, email FROM teachers ORDER BY id ASC"
