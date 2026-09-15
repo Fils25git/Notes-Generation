12ba0c84c9f14b06795185ea65e654a4b5613edb
Fils25git <fleduportal25@gmail.com>
2026-09-15 06:59:29 +0200

    Refactor to use ES6 module syntax in get-marks.js

--- functions/get-marks.js
+++ functions/get-marks.js
@@ -1,6 +1,6 @@
-const db = require("./db");
+import db from "./db.js";
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   try {
     const class_id = event.queryStringParameters?.class_id;
     const subject_id = event.queryStringParameters?.subject_id;
